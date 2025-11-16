18b5f191ea3a92c13758da0134da3af7a2aa56be
Tabuke Ezekiel <tabukeezekiel9@gmail.com>
2025-11-16 05:58:11 +0100

    feat: The entire project

---
+++ src/message.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+
+import express from "express";
+import cron from "node-cron";
+import { sendEmail } from "./mailer/mailer.js";
+
+const { PORT, NODE_ENV } = process.env;
+
+const app = express();
+
+cron.schedule("0 21 * * *", sendEmail, { timezone: "Africa/Lagos" });
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${NODE_ENV} on port ${PORT}`);
+  console.log(
+    `Server deployed on ${new Date().toLocaleTimeString(["WAT"], {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "numeric",
+      hour12: true,
+    })}`
+  );
+});
