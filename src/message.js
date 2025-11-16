2b3e45bb868d7af7d7e472b90c67e3fca6326631
Ezekiel, the Full Stack Visionary <tabukeezekiel9@gmail.com>
2025-11-16 15:57:56 +0100

    Update message.js

--- src/message.js
+++ src/message.js
@@ -8,7 +8,7 @@ const { PORT, NODE_ENV } = process.env;
 
 const app = express();
 
-cron.schedule("00 56 15 * * *", sendEmail, { timezone: "Africa/Lagos" });
+cron.schedule("00 00 16 * * *", sendEmail, { timezone: "Africa/Lagos" });
 
 app.listen(PORT, () => {
   console.log(`Server running in ${NODE_ENV} on port ${PORT}`);
